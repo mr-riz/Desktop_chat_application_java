806a0194641fe331fd3aa8ac5eaf4cdca93c089f
RAHUL SHARMA <rahulsharmaofficial356@gmail.com>
2021-08-29 23:13:04 +0530

    Client code

---
+++ ChatApplication/src/Client.java
@@ -0,0 +1,13 @@
+/*
+ * This is Desktop Chat Application for windows
+ * rahulsharmaofficial356@gmail.com
+ * Project developed by RAHUL SHARMA
+ */
+public class Client {
+
+	public static void main(String[] args) {
+		System.out.println("i am client..... client program is going to start");
+
+	}
+
+}
