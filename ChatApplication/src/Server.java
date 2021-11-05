fc8088d6043f579fb6344331d46bc8d505e1a0fa
rahul <rahul@RAHUL-SHARMA>
2021-11-05 21:58:28 +0530

    server

---
+++ ChatApplication/src/Server.java
@@ -0,0 +1,120 @@
+import java.net.*;
+import java.io.*; 
+
+public class Server {
+	ServerSocket server;
+	Socket socket;
+	
+	BufferedReader br;
+	PrintWriter out;
+	
+	//constructor.....
+	 public Server()
+	{
+		try
+		{
+			server=new ServerSocket(7777);
+			System.out.println("Server is waiting to accept connection");
+			System.out.println("waiting.......");
+			socket =server.accept();
+			
+			br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out=new PrintWriter(socket.getOutputStream());
+			startReading();
+			startWriting();
+			
+			
+			
+		}
+		catch(Exception e)
+		{
+			//e.printStackTrace();
+			 System.out.println("connection is closed");
+			
+			
+		}
+		
+	}
+	 public void startReading()
+	 {  
+		 //thread-read karke deta rahega
+		  Runnable r1=()->{
+			  //lambda function
+			  
+			  System.out.println("reader started..");
+			  try {
+			  while(true)
+			  {
+				  
+					  String msg=br.readLine();
+				  if(msg.equals("exit"))
+				  {
+					  System.out.println("client terminated the chat");
+					 // socket.close();
+					  break;
+					 
+				  }
+					  System.out.println("Client :"+msg);
+					 
+				  
+			  }
+			  }
+			  
+			  catch(Exception e)
+			  {
+				  //e.printStackTrace();
+				  System.out.println("connection is closed");
+			  }
+			
+		  };
+		  
+		  new Thread(r1).start();
+		 
+	 }
+	 public void startWriting()
+	 {
+		 // thread - data user lega and the send karega client tak
+		 Runnable r2=()->{
+			 
+			 System.out.println("writer started");
+			  //lambda function
+			 try {
+			 
+			 while(true)
+			 {
+				 
+					
+					 BufferedReader br1=new BufferedReader(new InputStreamReader(System.in));
+					 String content=br1.readLine();
+					 
+					 out.println(content);
+					 out.flush();
+				 if(content.equals("exit"));
+				 {
+						 //socket.close();
+					 break;
+				 }
+					 
+					 
+				
+			 }
+			 }
+			 catch(Exception e)
+			 {
+				 //e.printStackTrace();
+				 System.out.println("connection is closed");
+			 }
+			  
+		  };
+		  new Thread(r2).start();
+	 }
+	public static void main(String[] args) {
+		
+	 System.out.println("this is server......going to start server.");
+	 new Server();
+	 
+	 
+
+	}
+
+}
